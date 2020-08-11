7a5672e501ae0683cbffee1157434c0021f44827
MatijaMasek <58993276+MatijaMasek@users.noreply.github.com>
2020-08-11 22:30:48 +0200

    Add files via upload

---
+++ Inventory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public Text treeText;
+    public Text foodText;
+    public Text workersText;
+    public Text coinsText;
+
+    public int tree;
+    public int food;
+    public int workers;
+    public int coins;
+
+    
+    void Update()
+    {
+        treeText.text = "Trees: " + tree.ToString("0");
+        foodText.text = "Food: " + food.ToString("0");
+        workersText.text = "Workers: " +  workers.ToString("0");
+        coinsText.text = "Coins: " +  coins.ToString("0");
+    }
+}
